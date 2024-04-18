9a8a148b664bf70494b51d71d143dbeb1a1b1a69
denisu <denisu14f@gmail.com>
2024-04-18 08:32:20 -0400

    path generation

--- testf.py
+++ testf.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import product, combinations
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,9 +7,21 @@ from matplotlib.widgets import Button
 def posgen(G):
     ret = {}
     for n in G:
+        #print(n)
+        #print(G.nodes)
         ret[n] = [G.nodes[n]["x"],G.nodes[n]["y"]]
     return ret
-
+def colgen(G):
+    ret = []
+    #print(len(G.edges),"aaaaa")
+    for i,n in enumerate(G.edges):
+        #print(n)
+        #print(G.edges)
+        if n[0] <= -1 or n[1] <= -1:
+            ret.append('b')
+        else:
+            ret.append('lightgreen')
+    return ret
 G = nx.Graph()
 
 data1 = [(0,{"x": 0, "y": 0}),
@@ -36,7 +48,36 @@ data2 = [(0,1),
          (7,8)
          ]
 
+def which_paths(x : int):
+    return [True] * x
+    
 
+def create_path(G: nx.Graph, tree: nx.Graph, width: float, fnx):
+    l_ed = []
+    AA = nx.Graph()
+    for u,v in G.edges:
+        if not tree.has_edge(u,v):
+            l_ed.append((u,v))
+    ch_b = [True] * len(l_ed)
+    ch_b = which_paths(len(l_ed))
+    for i in range(len(l_ed)):
+        #u = l_ed[i][0]
+        #v = l_ed[i][1]
+        if ch_b[i] == True:
+            unx = np.array([G.nodes[l_ed[i][0]]["x"]-G.nodes[l_ed[i][1]]["x"],G.nodes[l_ed[i][0]]["y"]-G.nodes[l_ed[i][1]]["y"]])
+            unx_hat = unx / np.linalg.norm(unx)
+            G.add_node(-(i+1), **({"x": (unx_hat*width)[0]+G.nodes[l_ed[i][1]]["x"], "y": (unx_hat*width)[1]+G.nodes[l_ed[i][1]]["y"]}) )
+            G.add_edge(l_ed[i][1], -(i+1))
+        else:
+            unx = np.array([G.nodes[l_ed[i][1]]["x"]-G.nodes[l_ed[i][0]]["x"],G.nodes[l_ed[i][1]]["y"]-G.nodes[l_ed[i][0]]["y"]])
+            unx_hat = unx / np.linalg.norm(unx)
+            G.add_node(-(i+1), **({"x": (unx_hat*width)[0]+G.nodes[l_ed[i][0]]["x"], "y": (unx_hat*width)[1]+G.nodes[l_ed[i][0]]["y"]}) )
+            G.add_edge(l_ed[i][0], -(i+1))
+    #t_p = list(nx.dfs_edges(tree, source=0))
+        
+def create_gcode():
+    pass     
+        
 def n_stree(G) -> int:
     if not nx.is_connected(G):
         return -1
@@ -84,31 +125,35 @@ G.add_nodes_from(data1)
 G.add_edges_from(data2)
 
 print("Num of Possible Unique Spanning Trees:", n_stree(G))
-print(len(G.edges))
-print(len(G.nodes))
-
+#print(len(G.edges))
+#print(len(G.nodes))
+print("DBG: ", end='')
 
 all_span = spanning_trees(G)
-print(all_span)
+create_path(G, all_span[82], 0.2, which_paths)
+print(G.nodes)
+#print(all_span)
 
 pos = posgen(G)
 
 
-ccxc = 0
+ccxc = 82
 
-def onclickA(event):
-    global ccxc
-    plt.clf()
-    nx.draw(G, pos, with_labels=True, node_color='blue', node_size=200, edge_color='b')
-    nx.draw(all_span[ccxc], pos, with_labels=True, node_color='green', node_size=200, edge_color='r')
-    ccxc += 1
-    plt.show() 
+# def onclickA(event):
+#     global ccxc
+#     global bnbtn
+#     global bax
+#     plt.clf()
+#     nx.draw(G, pos, with_labels=True, node_color='blue', node_size=200, edge_color='b')
+#     nx.draw(all_span[ccxc], pos, with_labels=True, node_color='green', node_size=200, edge_color='r')
+#     ccxc += 1
+#     plt.show() 
 
-fig,ax = plt.subplots()
-bax = fig.add_axes([0.81, 0.05, 0.1, 0.075])
-bnbtn = Button(bax, "")
-bnbtn.on_clicked(onclickA)
+# fig,ax = plt.subplots()
+# bax = fig.add_axes([0.05, 0.05, 0.2, 0.15])
+# bnbtn = Button(bax, "test")
+# bnbtn.on_clicked(onclickA)
 
-#nx.draw(G, pos, with_labels=True, node_color='blue', node_size=200, edge_color='b')
-#nx.draw(all_span[0], pos, with_labels=True, node_color='green', node_size=200, edge_color='r')
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=200, edge_color=colgen(G))
+nx.draw(all_span[82], pos, with_labels=True, node_color='green', node_size=200, edge_color='r')
 plt.show()
