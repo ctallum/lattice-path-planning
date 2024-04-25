fe8589b7a562da47a3aacf79ff7afe540f9fd9e8
denisu <denisu14f@gmail.com>
2024-04-25 10:12:32 -0400

    full enumeration

--- testf.py
+++ testf.py
@@ -4,6 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 from slicer import Slicer
+from random import random
+
+def uvec(vector):
+    return vector / np.linalg.norm(vector)
+def angle_between(v1, v2):
+    v1_u = uvec(v1)
+    v2_u = uvec(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
 
 def posgen(G):
     ret = {}
@@ -23,6 +33,56 @@ def colgen(G):
         else:
             ret.append('darkblue')
     return ret
+def angC(G: nx.Graph, a: tuple, b:tuple):
+    print(a,b, "CALLED ANGC")
+    rr = tuple(set(a) & set(b))
+    print(rr, "TWO EDGES")
+    rra1 = invr(a.index(rr[0]))
+    rrb1 = invr(b.index(rr[0]))
+    print(rra1, "rra1", type(rra1))
+    print(rrb1, "rrb1")
+    ooox = np.array([G.nodes[rr[0]]["x"], G.nodes[rr[0]]["y"]])
+    p1 = np.array([G.nodes[a[rra1]]["x"], G.nodes[a[rra1]]["y"]])
+    p2 = np.array([G.nodes[b[rrb1]]["x"], G.nodes[b[rrb1]]["y"]])
+    print(p1,ooox,p2)
+    p1=(p1-ooox)
+    p2=(ooox-p2)
+    print(p1,p2,"p1p2")
+    p1=uvec(p1)
+    p2=uvec(p2)
+    #angle = np.arctan2(p1,p2)[0]
+    angle = np.arctan2(np.cross(p1, p2), np.dot(p1, p2))
+    print(angle)
+    return angle
+
+
+    
+
+def outb(a: list, b: tuple):
+    print(a, "BEFORE REM")
+    print(b, "tuple to be removed")
+    if b == None: return a
+    c=(b[1],b[0])
+    if b in a:
+        a.remove(b)
+        return a.copy()
+    elif c in a:
+        a.remove(c)
+        return a.copy()
+    else: return a.copy()
+def gover(a: tuple, b: int):
+    if a[0] == b:
+        return a[1]
+    else: return a[0]
+
+def invr(a: int):
+    if a==1:
+        return 0
+    elif a==0:
+        return 1
+        
+    
+    
 G = nx.Graph()
 
 data1 = [(0,{"x": 0, "y": 0}),
@@ -33,7 +93,9 @@ data1 = [(0,{"x": 0, "y": 0}),
          (5,{"x": 2, "y": 1}),
          (6,{"x": 0, "y": 2}),
          (7,{"x": 1, "y": 2}),
-         (8,{"x": 2, "y": 2})]
+         (8,{"x": 2, "y": 2}),
+         (9,{"x": 3, "y":3})
+         ]
 
 data2 = [(0,1),
          (1,2),
@@ -46,7 +108,9 @@ data2 = [(0,1),
          (4,7),
          (5,8),
          (6,7),
-         (7,8)
+         (7,8),
+         (7,9),
+         (8,9)
          ]
 
 def which_paths(x : int): #TODO optional way to pick a path out of 2^(x) paths
@@ -77,20 +141,60 @@ def create_path(G: nx.Graph, tree: nx.Graph, width: float, fnx):
             unx_hat = unx / np.linalg.norm(unx)
             G.add_node(-(i+1), **({"x": (unx_hat*width)[0]+G.nodes[l_ed[i][0]]["x"], "y": (unx_hat*width)[1]+G.nodes[l_ed[i][0]]["y"]}) )
             G.add_edge(l_ed[i][1], -(i+1))
-    t_p = list(nx.dfs_edges(G, source=0))
-    print(t_p)
-    ff_path = []
-    for i in range(len(t_p)):
-        obn_a = []
-        crx = t_p[i]
-        crxt = crx[1]
-        CX=0
-        for edg in t_p:
-            CX+=1
-            if (edg[0]==crxt or edg[1]==crxt) and (edg != crx):
-                obn_a.append(edg)
-                print(obn_a, crx, CX)
-        ff_path.append(sortwith_edgeangles(obn_a))
+    G.remove_edges_from(l_ed)
+    traj = []
+    sn = 0 #sn
+    crnn = sn
+    cuee = G.edges(sn)
+    icm_e = None
+    LEFTORRIGHT = True # true if left
+    ixix = 0
+    lastcycle = False
+    while (True and ixix < 1200):
+        print(ixix)
+        if lastcycle: break
+        if crnn == sn and ixix>4: lastcycle = True
+        print(traj, "TRAJ\n")
+        ava_e = outb(list(cuee), icm_e)
+        print(ava_e, "avae")
+        print(icm_e, "icme")
+        if icm_e == None:
+            traj.append(crnn)
+            crnn = gover(ava_e[0],crnn)
+            cuee = G.edges(crnn)
+            icm_e = ava_e[0]
+            continue
+        if len(ava_e) == 0: 
+            LEFTORRIGHT = not LEFTORRIGHT
+            traj.append(crnn)
+            traj.append("TURN")
+            print("TURNING")
+            icm_e = None
+            print(crnn, "crnn val rn")
+            print(icm_e, "icm val rn")
+            print(ava_e, "ava val rn")
+            continue
+              
+        anglepicks = np.zeros(len(ava_e))
+        for edge in range(len(ava_e)):
+            print(edge, "AAAAAAAA")
+            anglepicks[edge] = angC(G, ava_e[edge], icm_e)
+        print(anglepicks, "ANGLE PICKS")
+        print(ava_e, "AVAE AFTER")
+        if LEFTORRIGHT:
+            indextogo = anglepicks.argmin()
+        else:
+            indextogo = anglepicks.argmin()
+        traj.append(crnn)
+        crnn = gover(ava_e[indextogo], crnn)
+        cuee = G.edges(crnn)
+        icm_e = ava_e[indextogo]
+        ixix+=1
+    print(traj, "BX")
+        
+        
+        
+    
         
 
 def create_gcode(): #TODO
